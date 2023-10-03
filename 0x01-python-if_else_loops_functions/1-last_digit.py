b756af0a6f496f1b750fb8afaceef92af91dbe9e
mistidevs-alx <mistidevs@gmail.com>
2023-10-03 05:01:41 -0700

    Determining the last digit of integer based on bands

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+
+def last_digit(num):
+    return num - (10 * int(num / 10))
+
+
+last = last_digit(number)
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+if last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+if last < 6 and last != 0:
+    print(f"Last digit of {number} is {last} and is "
+          "less than 6 and not 0")
