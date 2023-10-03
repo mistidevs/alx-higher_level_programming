168adc5d778ae345bdb07a20790a7c20c8bd7f39
mistidevs-alx <mistidevs@gmail.com>
2023-10-03 12:07:40 -0700

    Handling negative input

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    n_str = str[:n] + str[n+1:]
-    return n_str
+    if n >= 0:
+        n_str = str[:n] + str[n+1:]
+        return n_str
+    else:
+        return str
