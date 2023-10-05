44aae3bbbec9838ca7c83df6b0a7c628e343fe93
mistidevs-alx <mistidevs@gmail.com>
2023-10-05 04:52:40 -0700

    Adding case where there is one argument and Pycodestyling

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,14 +3,19 @@ from sys import argv
 
 len = len(argv) - 1
 
+
 def print_args():
-    print("{} arguments".format(len), end="")
+    if len != 1:
+        print("{} arguments".format(len), end="")
+    else:
+        print("1 argument", end="")
     if len == 0:
         print(".")
     else:
         print(":")
         for i in range(1, len + 1):
             print("{}: {}".format(i, argv[i]))
+
 
 if __name__ == '__main__':
     print_args()
