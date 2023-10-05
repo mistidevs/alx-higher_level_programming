aafdd8c58ef3ee59751aa2cfe1529fd13f78ec2c
mistidevs-alx <mistidevs@gmail.com>
2023-10-05 04:53:21 -0700

    Pycodestyling

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,13 @@ from sys import argv
 
 len = len(argv) - 1
 
+
 def add_infinite():
     result = 0
     for i in range(1, len + 1):
         result += int(argv[i])
     print("{:d}".format(result))
+
 
 if __name__ == '__main__':
     add_infinite()
