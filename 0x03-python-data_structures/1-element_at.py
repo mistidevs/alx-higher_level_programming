85e2d2a8d52e04d8995a714218240fe2ada5a922
mistidevs-alx <mistidevs@gmail.com>
2023-10-08 00:35:02 -0700

    Code optimisation

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    length = len(my_list)
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return None
-    if idx > (length - 1):
-        return None
-    for i in my_list:
-        if i == idx:
-            return my_list[i]
+    return my_list[idx]
 
 
 if __name__ == '__main__':
