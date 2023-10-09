286ae111202f34147ba14ec808d9951cfe51c2bb
mistidevs-alx <mistidevs@gmail.com>
2023-10-09 05:16:51 -0700

    Handling edge case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return
+        return (0, None)
     length = len(sentence)
     char = sentence[0]
     return (length, char)
