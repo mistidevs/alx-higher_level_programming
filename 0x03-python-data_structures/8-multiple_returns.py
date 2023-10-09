2b835b16a902f397801a6a1fba1855ce8f36cda4
mistidevs-alx <mistidevs@gmail.com>
2023-10-09 04:55:34 -0700

    Using tuples to make multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return
+    length = len(sentence)
+    char = sentence[0]
+    return (length, char)
+
+
+if __name__ == '__main__':
+    multiple_returns()
