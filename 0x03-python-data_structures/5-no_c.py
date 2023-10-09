8a9d8691a3d6b0a3835318cffb1feeda5333092c
mistidevs-alx <mistidevs@gmail.com>
2023-10-09 04:51:05 -0700

    Handling empty strings by returning them

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if not my_string:
-        return
+        return my_string
     new_string = ""
     for char in my_string:
         if char != 'c' and char != 'C':
