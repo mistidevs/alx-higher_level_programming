9af3f856c54e453a9e7892b18c8e0bf0baa8b34c
mistidevs-alx <mistidevs@gmail.com>
2023-10-10 03:08:23 -0700

    Checking if a list is a palindrome in linearithmic time and O(1) space

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,4 +7,43 @@
 * Return: integer
 */
 
+int is_palindrome(listint_t **head)
+{
+listint_t *slow, *fast, *prev, *previous, *current, *next, *left, *right;
 
+if (*head == NULL)
+	return (1);
+
+slow = fast = *head;
+while (fast != NULL)
+{
+	prev = slow;
+	fast = fast->next->next;
+	slow = slow->next;
+}
+if (*head == slow)
+	return (1);
+prev->next = NULL;
+
+previous = NULL;
+current = slow;
+while (current != NULL)
+{
+	next = current->next;
+	current->next = previous;
+	previous = current;
+	current = next;
+}
+
+left = *head;
+right = previous;
+while (right != NULL || left != NULL)
+{
+	if (right->n != left->n)
+		return (0);
+	right = right->next;
+	left = left->next;
+}
+
+return (1);
+}
