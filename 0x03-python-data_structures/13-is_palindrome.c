2688fddbf3c697b9113aa58d7fe9eae931202889
mistidevs-alx <mistidevs@gmail.com>
2023-10-10 03:33:59 -0700

    Using a helper function and reversing the list back

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,30 @@
 #include "lists.h"
 
 /**
+* reverse_list - helper function to reverse a list
+* @head: pointer to pointer to start on the list to reverse
+*
+* Return: pointer to listint_t
+*/
+
+listint_t *reverse_list(listint_t **head)
+{
+listint_t *prev = NULL;
+listint_t *curr = *head;
+listint_t *next;
+
+while (curr != NULL)
+{
+	next = curr->next;
+	curr->next = prev;
+	prev = curr;
+	curr = next;
+}
+*head = prev;
+return (*head);
+}
+
+/**
 * is_palindrome - Checking if linked list is palindrome
 * @head: pointer to pointer to first node of linked list
 *
@@ -9,41 +33,35 @@
 
 int is_palindrome(listint_t **head)
 {
-listint_t *slow, *fast, *prev, *previous, *current, *next, *left, *right;
+listint_t *slow = *head;
+listint_t *fast = *head;
+listint_t *half;
+listint_t *temp;
 
-if (*head == NULL)
+if (head == NULL || *head == NULL)
 	return (1);
 
-slow = fast = *head;
-while (fast != NULL)
+while (fast != NULL && fast->next != NULL)
 {
-	prev = slow;
-	fast = fast->next->next;
 	slow = slow->next;
-}
-if (*head == slow)
-	return (1);
-prev->next = NULL;
-
-previous = NULL;
-current = slow;
-while (current != NULL)
-{
-	next = current->next;
-	current->next = previous;
-	previous = current;
-	current = next;
+	fast = fast->next->next;
 }
 
-left = *head;
-right = previous;
-while (right != NULL || left != NULL)
+if (fast != NULL)
+	slow = slow->next;
+
+half = reverse_list(&slow);
+temp = half;
+
+while (half != NULL)
 {
-	if (right->n != left->n)
+	if (half->n != (*head)->n)
 		return (0);
-	right = right->next;
-	left = left->next;
+	half = half->next;
+	*head = (*head)->next;
 }
+
+reverse_list(&temp);
 
 return (1);
 }
