b0a823963244c15d31d69635247634fa83cabb03
mistidevs-alx <mistidevs@gmail.com>
2023-10-11 12:13:26 -0700

    Barebones of 103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+
+void print_python_bytes(PyObject *p);
+
+/**
+* print_python_list - printing a PyObject List
+* @p: pointer to PyObject
+*
+* Return: void
+*/
+
+void print_python_list(PyObject *p);
+	exit(1);
+
+
+/**
+* print_python_bytes - printing Bytes object bytes
+* @p: pointer to PyObject
+*
+* Return: void
+*/
+
+void print_python_bytes(PyObject *p);
+	exit(1);
