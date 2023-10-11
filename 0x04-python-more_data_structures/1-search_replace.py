13f5fbfd67628c8707e4f45494df1b439ba226be
mistidevs-alx <mistidevs@gmail.com>
2023-10-11 10:27:15 -0700

    Creating a new list with  modified values in the search and replace function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,13 @@
 def search_replace(my_list, search, replace):
     if my_list is None or search is None or replace is None:
         return my_list
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    new_list = []
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list
 
 
 if __name__ == '__main__':
