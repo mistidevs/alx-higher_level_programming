ff9682b86b4d51b31f86654c16648019cb709ef9
mistidevs-alx <mistidevs@gmail.com>
2023-10-15 02:42:52 -0700

    Handling cases where roman_string is None or not a string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string == None or not isinstance(roman_string, str):
+        return 0
     res = 0
     pos = 0
     rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
