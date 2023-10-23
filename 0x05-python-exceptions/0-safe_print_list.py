745e1da649db41bd385eb9e4b15cfe09b8f55382
mistidevs-alx <mistidevs@gmail.com>
2023-10-23 11:08:00 -0700

    Printing a list in safe mode

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="");
+            count += 1;
+        except IndexError:
+            break
+
+    print()
+    return count
+
+
+if __name__ == '__main__':
+    safe_print_list()
+
