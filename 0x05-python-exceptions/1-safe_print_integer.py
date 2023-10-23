92c1eb35a5e73d90413a19cc64d1553aa5402bb2
mistidevs-alx <mistidevs@gmail.com>
2023-10-23 11:40:47 -0700

    Exception optimisations for 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,10 +3,9 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+        return True
+    except (ValueError, TypeError):
         return False
-
-    return True
 
 
 if __name__ == '__main__':
