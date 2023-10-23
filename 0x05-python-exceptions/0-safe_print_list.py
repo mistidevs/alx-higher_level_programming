c49212ed54918b081e959d3cea9b125f6c872469
mistidevs-alx <mistidevs@gmail.com>
2023-10-23 11:17:27 -0700

    Pycodestyling for type printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print(my_list[i], end="");
-            count += 1;
+            print(my_list[i], end="")
+            count += 1
         except IndexError:
             break
 
@@ -15,4 +15,3 @@ def safe_print_list(my_list=[], x=0):
 
 if __name__ == '__main__':
     safe_print_list()
-
