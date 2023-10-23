d70d791ba642b47aa62e2b35567217ffd5bb9907
mistidevs-alx <mistidevs@gmail.com>
2023-10-23 12:24:14 -0700

    Printing to stderr with sys

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as ve:
+        print("Exception: {}".format(ve), file=sys.stderr)
+        return False
+    except TypeError:
+        return False
+
+
+if __name__ == '__main__':
+    safe_print_integer_err()
