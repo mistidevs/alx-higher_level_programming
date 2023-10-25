aa38110d4a0b892aec93980e3a88a1e7cd094914
mistidevs-alx <mistidevs@gmail.com>
2023-10-25 03:10:16 -0700

    Printing space before stderr

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -50,6 +50,7 @@ class Square:
 
     def __str__(self):
         if self.__size == 0:
+            print()
             return None
         result = "\n" * self.__position[1]
         for i in range(self.__size):
