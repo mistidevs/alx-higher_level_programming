4191cec7c0f88ed44d0b657733de19f853f2023d
mistidevs-alx <mistidevs@gmail.com>
2023-11-02 11:13:26 -0700

    String Indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+""" Formatting text """
+
+
+def text_indentation(text):
+    """ Printing a string with conditional formatting """
+    if not isinstance(text, (str)):
+        raise TypeError("text must be a string")
+
+    end_characters = ['.', '?', ':']
+    lines = []
+    current_line = ""
+
+    for char in text:
+        current_line += char
+        if char in end_characters:
+            lines.append(current_line.strip())
+            lines.append("\n\n")
+            current_line = ""
+
+    lines.append(current_line.strip())
+
+    for line in lines:
+        print(line, end="")
