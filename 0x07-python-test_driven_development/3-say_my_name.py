45fb2c04b532b2ecba029e23b519a060703a01b1
mistidevs-alx <mistidevs@gmail.com>
2023-11-02 10:16:13 -0700

    Printing a name with formatting

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+""" Printing a name """
+
+
+def say_my_name(first_name, last_name=""):
+    """ Printing a name with formatting """
+    if not isinstance(first_name, (str)):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, (str)):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
