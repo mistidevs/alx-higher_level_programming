0f2a0ba67a03ce827659bd036acf76a738ec9937
mistidevs-alx <mistidevs@gmail.com>
2023-11-03 08:40:54 -0700

    Test suite for 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 matrix_mul = __import__('100-mat_mul').matrix_mul
 
+matrixA = [[1, 2, 3], [3, 4, 5], [5, 6, 7]]
+matrixB = [[1, 2, 3], [4, 5, 6]]
+
+print(matrix_mul(matrixB, matrixA))
 print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
 print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
 print(matrix_mul("Holberton", [[3, 4], [5, 6]]))
