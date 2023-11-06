d7a9b52c50a6c443ab551dff42d74b06450409e1
mistidevs-alx <mistidevs@gmail.com>
2023-11-06 08:58:13 -0800

    Function that returns the methods and attributes of a function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Printing the modules and attributes of a class """
+
+
+def lookup(obj):
+    """ Using dir """
+    return dir(obj)
+
+
+if __name__ == '__main__':
+    lookup()
