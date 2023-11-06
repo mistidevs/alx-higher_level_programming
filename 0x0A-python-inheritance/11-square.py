3ebced357a6efac9e8d79728adcb24a8bd27996e
mistidevs-alx <mistidevs@gmail.com>
2023-11-06 11:01:36 -0800

    Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Rectangle class derived from the BaseGeometry class """
+
+
+class BaseGeometry:
+    """ Base Geometry with Exception for area function """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ Class derived from BaseGeometry """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
+
+
+class Square(Rectangle):
+    """ Square sub class of Rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
