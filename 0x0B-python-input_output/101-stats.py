ab0e842cd0f15d60c4b0a0740288a80ac8be6694
mistidevs-alx <mistidevs@gmail.com>
2023-11-08 10:20:57 -0800

    Log parsing revert to first

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python3
-'''Module for log parsing script.'''
+"""101-stats.py"""
+
+
 import sys
 
-if __name__ == "__main__":
-    size = [0]
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+totalFileSize = 0
+statusCodeCounts = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0,
+}
+lineCount = 0
 
-    def check_match(line):
-        '''Checks for regexp match in line.'''
-        try:
-            line = line[:-1]
-            words = line.split(" ")
-            size[0] += int(words[-1])
-            code = int(words[-2])
-            if code in codes:
-                codes[code] += 1
-        except Exception:
-            pass
+try:
+    for line in sys.stdin:
+        parts = line.split()
+        if len(parts) >= 9:
+            statusCode = int(parts[-2])
+            fileSize = int(parts[-1])
 
-    def print_stats():
-        '''Prints accumulated statistics.'''
-        print("File size: {}".format(size[0]))
-        for k in sorted(codes.keys()):
-            if codes[k]:
-                print("{}: {}".format(k, codes[k]))
-    i = 1
-    try:
-        for line in sys.stdin:
-            check_match(line)
-            if i % 10 == 0:
-                print_stats()
-            i += 1
-    except KeyboardInterrupt:
-        print_stats()
+            totalFileSize += fileSize
+            statusCodeCounts[statusCode] += 1
+            lineCount += 1
+
+        if lineCount % 10 == 0:
+            print("File size: {}".format(totalFileSize))
+            for code in sorted(statusCodeCounts.keys()):
+                if statusCodeCounts[code] > 0:
+                    print("{}: {}".format(code, statusCodeCounts[code]))
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    print("File size: {}".format(totalFileSize))
+    for code in sorted(statusCodeCounts.keys()):
+        if statusCodeCounts[code] > 0:
+            print("{}: {}".format(code, statusCodeCounts[code]))
