5b8107d59af47ebd125d7a38f7b648f7dd285c2d
mistidevs-alx <mistidevs@gmail.com>
2023-11-08 10:45:57 -0800

    Importing rather than redefining

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,31 +2,7 @@
 """ Rectangle class derived from the BaseGeometry class """
 
 
-class BaseGeometry:
-    """ Base Geometry with Exception for area function """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """ Class derived from BaseGeometry """
-    def __init__(self, width, height):
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return (f"[Rectangle] {self.__width}/{self.__height}")
+Rectangle = __import__('8-rectangle').Rectangle
 
 
 class Square(Rectangle):
