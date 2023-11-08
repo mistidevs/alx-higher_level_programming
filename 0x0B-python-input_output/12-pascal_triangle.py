4ad4b23fa7be09a56af62382288b9ed5db02a936
mistidevs-alx <mistidevs@gmail.com>
2023-11-08 04:40:21 -0800

    Printing Pascals Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Returning a list of lists with the Pascal's triangle """
+
+
+def pascal_triangle(n):
+    """ Printing the pascals triangle with iterations """
+    if n <= 0:
+        return [[]]
+
+    pascals = [[] for _ in range(n)]
+    if n >= 1:
+        pascals[0].append(1)
+    if n >= 2:
+        pascals[1].append(1)
+        pascals[1].append(1)
+
+    row = 1
+    if n >= 3:
+        for i in range(n - 2):
+            pascals[row + 1].append(1)
+            for j in range(len(pascals[row]) - 1):
+                pascals[row + 1].append(pascals[row][j] + pascals[row][j + 1])
+            pascals[row + 1].append(1)
+            row += 1
+
+    return pascals
+
+
+if __name__ == '__main__':
+    pascal_triangle()
