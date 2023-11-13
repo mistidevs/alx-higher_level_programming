2195b2812935f1e8d870149b99705aa0c4412065
mistidevs <mistidevs@gmail.com>
2023-11-13 19:47:38 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-""" Class rectangle that inherits from Base """
+"Class rectangle that inherits from Base"
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    Rectangle class
-    Inherits from Base
-    """
+    "Rectangle class inheriting from Base"
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
