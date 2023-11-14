ddd6b5d9d0b83e6b8865c66ccad53c411e5e64db
Mistidevs Turtle Python <mistidevs@gmail.com>
2023-11-14 09:55:37 -0800

    Square Display and Area

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,3 +1,5 @@
+import io
+import sys
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -27,3 +29,30 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(ValueError):
             s11 = Square(4, 5, -2)
         
+    def test_area(self):
+        s12 = Square(3)
+        self.assertEqual(s12.area(), 9)
+
+    def test_display(self):
+        s13 = Square(2)
+        buf = io.StringIO()
+        sys.stdout = buf
+        s13.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(buf.getvalue(), '##\n##\n')
+
+    def test_display_x(self):
+        s14 = Square(2, 2)
+        buf = io.StringIO()
+        sys.stdout = buf
+        s14.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(buf.getvalue(), '  ##\n  ##\n')
+
+    def test_display_x_y(self):
+        s15 = Square(3, 1, 3)
+        buf = io.StringIO()
+        sys.stdout = buf
+        s15.display()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(buf.getvalue(), '\n\n\n ###\n ###\n ###\n')
