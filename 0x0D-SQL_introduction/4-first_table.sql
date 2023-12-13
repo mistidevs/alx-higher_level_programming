f59769a0fbaf42075da3700ed93fb5c6884fa5d1
mistidevs-alx <mistidevs@gmail.com>
2023-12-13 06:29:06 -0800

    Full Description

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creating a table
+-- in the database highlighted
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Describing a table
+-- without DESCRIBE and EXPLAIN
+SHOW CREATE TABLE first_table;
