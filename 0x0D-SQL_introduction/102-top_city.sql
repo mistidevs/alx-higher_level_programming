0bc00b180e881ae4d694492ae9bb6ce1a2ed9e17
mistidevs-alx <mistidevs@gmail.com>
2023-12-13 08:02:36 -0800

    Temperatures #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- Returning the top 3 cities
+-- based on avergae temperature
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
