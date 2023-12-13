e5e16cfcb504ca8a08e7200980ec16baf69e6344
mistidevs-alx <mistidevs@gmail.com>
2023-12-13 07:44:19 -0800

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Converting the database to 
+-- utf8 encoding
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
