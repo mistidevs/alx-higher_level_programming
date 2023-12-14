41de54eb49cc0cd6966ae477220fece745ca0bed
mistidevs-alx <mistidevs@gmail.com>
2023-12-14 11:30:03 -0800

    Rotten Toamtoes

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- Displaying the sum of ratings
+-- for movies
+SELECT s.title, SUM(r.rate) AS rating
+FROM tv_shows AS s
+INNER JOIN tv_show_ratings AS r
+ON s.id = r.show_id
+GROUP BY s.title
+ORDER BY rating DESC;
