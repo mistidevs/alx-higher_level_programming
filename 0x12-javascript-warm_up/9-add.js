a61df26d7cae01fbfff7d6b600e0c4ec4f10d726
mistidevs-node <mistidevs@gmail.com>
2024-01-09 10:55:02 -0800

    Ibid

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
 function add (a, b) {
   return a + b;
 }
-
-const firstNum = parseInt(argv[2]);
-const secondNum = parseInt(argv[3]);
+const firstNum = parseInt(process.argv[2]);
+const secondNum = parseInt(process.argv[3]);
 const sum = add(firstNum, secondNum);
 
 console.log(sum);
