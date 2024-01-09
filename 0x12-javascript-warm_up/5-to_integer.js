ea83f0ddc9ca1b6454cf48efa68df87633b5b0d7
mistidevs-node <mistidevs@gmail.com>
2024-01-09 10:51:11 -0800

    semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const firstArg = process.argv[2];
 const number = parseInt(firstArg);
 if (!isNaN(number)) {
- console.log(`My number: ${number}`);
+  console.log(`My number: ${number}`);
 } else {
- console.log('Not a number');
+  console.log('Not a number');
 }
