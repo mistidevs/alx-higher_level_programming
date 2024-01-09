70002fb771b82f10c683ddc54fcdc96a5c3283dc
mistidevs-node <mistidevs@gmail.com>
2024-01-09 09:15:07 -0800

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const char = 'X';
+const { argv } = require('node:process');
+const size = parseInt(argv[2]);
+
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += char;
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
