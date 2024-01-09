23cb86477730beb0caaedd17994bcbcd9d29c8c5
mistidevs-node <mistidevs@gmail.com>
2024-01-09 11:00:32 -0800

    Mapping

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  let firstBiggest = -Infinity;
-  let secondBiggest = -Infinity;
-
-  for (let i = 2; i < process.argv.length; i++) {
-    const num = parseInt(process.argv[i]);
-
-    if (num > firstBiggest) {
-      secondBiggest = firstBiggest;
-      firstBiggest = num;
-    } else if (num > secondBiggest && num !== firstBiggest) {
-      secondBiggest = num;
-    }
-  }
-  console.log(secondBiggest);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
