9651834c1080d86d07ca444bba399b42e1adae20
mistidevs-node <mistidevs@gmail.com>
2024-01-09 10:49:57 -0800

    Deep in thought

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-const myNum = parseInt(argv[2]);
-
+const myNum = parseInt(process.argv[2]);
 if (!isNaN(myNum)) {
   for (let i = 0; i < myNum; i++) {
     console.log('C is fun');
