1a8212767ba1251ef628f81c6ca0d2c99aab5d9c
mistidevs-node <mistidevs@gmail.com>
2024-01-09 10:43:37 -0800

    Really

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length < 3) {
-  console.log('No Argument');
-} else if (argv.length < 4) {
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
