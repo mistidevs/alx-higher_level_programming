712930663ad9afa0817a5bd5c00943e351ea14ee
mistidevs-node <mistidevs@gmail.com>
2024-01-09 07:38:47 -0800

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (argv.length < 3) {
+  console.log('No Argument');
+} else if (argv.length < 4) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
