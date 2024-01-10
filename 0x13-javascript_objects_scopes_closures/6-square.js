481ca7ca9c70729aee7e83755f622e5298eb53f7
mistidevs-node <mistidevs@gmail.com>
2024-01-10 11:09:40 -0800

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let row;
+    if (c === undefined) {
+      c = 'X'
+    }
+    for (let i = 0; i < this.width; i++) {
+      row = '';
+      for (let j = 0; j < this.height; j++) {
+	row += c
+      }
+      console.log(row);
+    }
+  }
+};
