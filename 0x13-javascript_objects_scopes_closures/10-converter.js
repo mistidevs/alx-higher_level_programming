ffe5fc2beb18d636859d02e7f10c81fe002b42a5
mistidevs-node <mistidevs@gmail.com>
2024-01-10 22:04:45 -0800

    Converter fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,23 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function (number) {
-    if (base < 2 || base > 36) {
-      return 'Invalid base';
-    }
-
-    const digits = [];
-    const sign = number < 0 ? -1 : 1;
-    number = Math.abs(number);
-
-    while (number > 0) {
-      digits.push(number % base);
-      number = Math.floor(number / base);
-    }
-
-    if (sign === -1) {
-      digits.push('-');
-    }
-
-    return digits.reverse().map(digit => digit >= 10 ? String.fromCharCode(digit - 10 + 65) : digit).join('');
+  return function (nb) {
+    return nb.toString(base);
   };
 };
