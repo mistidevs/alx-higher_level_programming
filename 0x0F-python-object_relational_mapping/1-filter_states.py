7cbb381b4cd8ed200856d0218b30f5961ac61ab1
mistidevs-alx <mistidevs@gmail.com>
2024-01-14 00:18:32 -0800

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" Selects all states in the database """
+""" Selects all states in the database starting with N """
 
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    """ Gets all the states in the table """
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     db = MySQLdb.connect(user=user, password=password, database=database,
                          host='localhost', port=3306)
     c = db.cursor()
-    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    c.execute("""SELECT * FROM states WHERE
+ASCII(SUBSTRING(states.name, 1, 1)) = 78 ORDER BY states.id ASC""")
     while (row := c.fetchone()) is not None:
         print(row)
