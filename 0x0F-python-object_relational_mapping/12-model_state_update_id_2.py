d59a8267203c0f0c1944ccc601e3a3a5750ac9a8
mistidevs <mistidevs@gmail.com>
2024-01-14 14:23:11 +0300

    12 Fix #1

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,8 +7,11 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """ Using sessions to accomplish this """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@\
+localhost/{}'.format(user, password, database), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     session.query(State).filter_by(id=2).update({"name": "New Mexico"})
