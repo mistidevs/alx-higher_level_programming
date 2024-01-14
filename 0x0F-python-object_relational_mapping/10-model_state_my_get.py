50ab8d5f7092310d0c5cfb3bb72ee7e5faca8ca7
mistidevs-alx <mistidevs@gmail.com>
2024-01-14 03:21:50 -0800

    10 Fix #2

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,9 +8,10 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     """ Using sessions to accomplish this """
     user = sys.argv[1]
-    pass = sys.argv[2]
+    password = sys.argv[2]
     database = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user, pass, database), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@\
+localhost/{}'.format(user, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
