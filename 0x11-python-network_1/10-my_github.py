8f15bd154374674db92e0c59093d7d0ab3262a8c
Mistidevs Network #1 <mistidevs@gmail.com>
2024-01-26 01:14:03 -0800

    My Github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Querying Github credentials to get id
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    token = sys.argv[2]
+    r = requests.get('https://api.github.com/user', auth=(username, token))
+    if r.status_code == 200:
+        print(r.json().get('id'))
+    else:
+        print(None)
+              
