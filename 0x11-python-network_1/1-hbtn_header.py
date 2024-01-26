55f430b1e8bcc6efd613371fee97516cd32d11fb
Mistidevs Network #1 <mistidevs@gmail.com>
2024-01-26 00:29:03 -0800

    Header Value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Getting a header value of X-Request-Id
+"""
+import urllib.request
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv[1]) > 1:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.getheader('X-Request-Id'))
