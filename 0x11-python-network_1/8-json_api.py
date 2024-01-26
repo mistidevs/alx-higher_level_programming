a13a784c82fac87b80456ae17d3ea5c2b4e4f8fb
Mistidevs Network #1 <mistidevs@gmail.com>
2024-01-26 01:03:33 -0800

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+API Search with JSON
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+   if len(sys.argv) > 1:
+       q = sys.argv[1]
+   else:
+       q = ""
+
+   try:
+        response = requests.post('http://0.0.0.0:5000/search_user', data = {'q': q})
+        json_response = response.json()
+        if 'id' in json_response and 'name' in json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+   
+   except ValueError:
+        print("Not a valid JSON")
