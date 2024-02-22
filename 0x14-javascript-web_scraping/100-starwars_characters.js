4dac1f29ec05c8cc83aafdf43e7af329166078ea
mistidevs-node <mistidevs@gmail.com>
2024-02-22 08:27:25 -0800

    Using Promises in JS to implement async/await functionality

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-    console.error(error)
+    console.error(error);
     return;
   }
 
@@ -13,6 +13,10 @@ request(url, function (error, response, body) {
   for (let i = 0; i < characters.length; i++) {
     const character = characters[i];
     request(character, function (error, response, body) {
+      if (error) {
+        console.error(error);
+        return;
+      }
       const dataChar = JSON.parse(body);
       const name = dataChar.name;
       console.log(name);
