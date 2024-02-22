4ddae27dd3b4cfaf10d97bc972b80970834ae11d
mistidevs-node <mistidevs@gmail.com>
2024-02-22 09:36:28 -0800

    Semistandard for 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-function requestPromise(url) {
+function requestPromise (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) {
