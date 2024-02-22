14759cef42cb64ab1ffb54e441c1fabe52145c16
mistidevs-node <mistidevs@gmail.com>
2024-02-22 05:17:57 -0800

    Using reduce and endsWith functions

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-  let count = 0;
-  const data = JSON.parse(body);
-  const results = data.results;
-  for (let i = 0; i < results.length; i++) {
-    const episode = results[i];
-    const characters = episode.characters;
-    for (let j = 0; j < characters.length; j++) {
-      const character = characters[j];
-      if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
-        count += 1;
-      }
-    }
-  }
-  console.log(count);
 });
